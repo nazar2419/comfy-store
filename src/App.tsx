93425cbab09b7eeaf48d1e9a7b74d4fa748aae07
Nazar <nazarx095@gmail.com>
2024-04-23 20:06:28 +0300

    installing and configuring the redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Button } from "./components/ui/button";
+import { useAppSelector } from "./hooks";
+
 function App() {
-  return <div>h1</div>;
+  const { name } = useAppSelector((state) => state.userState);
+  console.log(name);  
+  return (
+    <>
+      <h1 className="text-7xl font-bold">h1</h1>;
+      <Button
+        variant="destructive"
+        size='lg'
+        onClick={() => console.log('it worked!!!')}
+      >
+        Click  me
+      </Button>
+    </>
+  )
 }
 
 export default App;
