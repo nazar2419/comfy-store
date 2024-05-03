c5b236782f358fe50adfc34adb8389691498e424
Nazar <nazarx095@gmail.com>
2024-05-03 15:54:49 +0300

    error page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,10 +4,8 @@ import { Armchair } from "lucide-react";
 function Logo() {
   return (
     <Link
-      to="/"
-      className="hidden lg:flex justify-center 
-      items-center bg-primary p-2 rounded-lg 
-      text-white "
+      to='/'
+      className='hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white'
     >
       <Armchair className="w-8 h-8" />
     </Link>
